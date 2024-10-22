40d7ca83a8e76d3631cfca745fea57ffbc03a014
pangao <pangao1990@qq.com>
2024-10-22 22:17:00 +0800

    修复找不到dmgbuild的问题

--- pyapp/package/dmg/dmg.py
+++ pyapp/package/dmg/dmg.py
@@ -1,7 +1,8 @@
-filename = 'PPX'
-volume_name = 'PPX.dmg'
+
+filename = 'PPX-V5.2.0_macOS'
+volume_name = 'PPX-V5.2.0_macOS.dmg'
 format = 'UDBZ'
-files = ['~/build/PPX.app', '~/pyapp/package/dmg/潘高的小站.webloc']
+files = ['/Users/lpjy/VScodeProjects/Python/PPX/PPX/pyapp/../build/PPX.app', '/Users/lpjy/VScodeProjects/Python/PPX/PPX/pyapp/package/dmg/潘高的小站.webloc']
 symlinks = {'Applications': '/Applications'}
 icon_locations = {
     'PPX.app': (160, 120),
@@ -11,5 +12,5 @@ icon_locations = {
 window_rect = ((200, 200), (590, 416))
 icon_size = 60
 text_size = 12
-badge_icon = '~/pyapp/icon/logo.icns'
-background = '~/pyapp/package/dmg/bg.png'
+badge_icon = '/Users/lpjy/VScodeProjects/Python/PPX/PPX/pyapp/icon/logo.icns'
+background = '/Users/lpjy/VScodeProjects/Python/PPX/PPX/pyapp/package/dmg/bg.png'
